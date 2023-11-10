556afb903345667e1bc93e36491727cb01ff6ccb
Tómas Ingi <tomasingi00@gmail.com>
2023-11-10 21:40:19 +0100

    Stockholm

--- backend/fetcher.py
+++ backend/fetcher.py
@@ -2,20 +2,44 @@ import json
 from OSMPythonTools.overpass import Overpass
 import osm2geojson
 
-def main():
+def get_buses_reykjavik(overpass):
     bbox = '63,-25,68,-13'
     query = ''.join([
         f'relation["type"="route"]["route"="bus"]({bbox});',
         f'out geom;'
     ])
-    overpass = Overpass()
     result = overpass.query(query)
 
-    path = '../scripts/output.js'
+    path = '../scripts/buses_reykjavik.js'
     with open(path, 'w') as f:
         geojson = osm2geojson.json2geojson(result.toJSON())
         f.write('var data = ')
         json.dump(geojson, f, indent=4)
+
+def get_sl(overpass):
+    bbox = '58.85,17.09,59.94,18.8'
+    query = ''.join([
+        f'(',
+        f'relation["type"="route"]["network"="SL"]["route"="train"]({bbox});',
+        f'relation["type"="route"]["network"="Stockholms tunnelbana"]["route"="subway"]({bbox});',
+        f'relation["type"="route"]["network"="SL"]["route"="light_rail"]({bbox});',
+        f'relation["type"="route"]["network"="SL"]["route"="tram"]({bbox});',
+        f');',
+        f'out geom;'
+    ])
+    result = overpass.query(query)
+
+    path = '../scripts/sl.js'
+    with open(path, 'w') as f:
+        geojson = osm2geojson.json2geojson(result.toJSON())
+        f.write('var data = ')
+        json.dump(geojson, f, indent=4)
+
+def main():
+    overpass = Overpass()
+
+    get_buses_reykjavik(overpass)
+    get_sl(overpass)
 
 if __name__ == '__main__':
     main()
--- scripts/base_map.js
+++ scripts/base_map.js
@@ -1,4 +1,16 @@
-var map = L.map('map').setView([64.12, -21.85], 11);
+const mapCenters = {
+    default: {pos: [64.12, -21.85], zoom: 11},
+    reykjavik: {pos: [64.12, -21.85], zoom: 11},
+    stockholm: {pos: [59.32, 18.06], zoom: 10},
+}
+
+const loc = location.pathname.split('/').slice(-1)[0].split('.')[0];
+const center = mapCenters[loc] || mapCenters.default;
+
+var map = L.map('map', {
+    zoomDelta: 1,
+    wheelPxPerZoomLevel: 120
+}).setView(center.pos, center.zoom);
 
 var tiles = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -7,5 +19,5 @@ var tiles = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}
     id: 'mapbox/streets-v11',
     tileSize: 512,
     zoomOffset: -1,
-    accessToken: 'pk.eyJ1IjoiZGFzc2NoZWl0IiwiYSI6ImNqa3IxeHZzMDBvYWgza3Q5OWM5MzUzcHUifQ.6xj-BznboqX4wPfbvnnEGA'
+    accessToken: 'pk.eyJ1IjoiZGFzc2NoZWl0IiwiYSI6ImNqa3IxeHZzMDBvYWgza3Q5OWM5MzUzcHUifQ.6xj-BznboqX4wPfbvnnEGA',
 }).addTo(map);
